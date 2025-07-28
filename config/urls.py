8e9887d3fc29f051f75494e42c457aeac57ff510
yunun0410 <yunun0410@gmail.com>
2025-07-28 23:11:50 +0900

    ✨feat: chapter_04 과제 업로드

--- config/urls.py
+++ config/urls.py
@@ -16,14 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import todo_list, todo_info
+from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', todo_list, name='todo_list'),
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path('todo/create/', todo_create, name='todo_create'),
+    path('todo/<int:todo_id>/update/', todo_update, name='todo_update'),
+    path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', user_views.login, name='login'),
-    path('accounts/signup', user_views.sign_up, name='signup'),
+    path('accounts/signup/', user_views.sign_up, name='signup'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,26 +1,58 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
 from todo.models import Todo
+from todo.forms import TodoForm, TodoUpdateForm
 
 def todo_list(request):
-    todo_list = Todo.objects.all().values_list('id', 'title')
-    result = [{'id': todo[0], 'title': todo[1]} for i, todo in enumerate(todo_list)]
+    todo_list = Todo.objects.filter(user=request.user).order_by('created_at')
+    q = request.GET.get('q')
+    if q:
+        todo_list = todo_list.filter(Q(title__icontains=q) | Q(description__icontains=q))
+    paginator = Paginator(todo_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj
+    }
+    return render(request, 'todo_list.html', context)
 
-    return render(request, 'todo_list.html', {'data': result})
-
-
+@login_required
 def todo_info(request, todo_id):
-    try:
-        todo = Todo.objects.get(id=todo_id)
-        info = {
-            'title': todo.title,
-            'description': todo.description,
-            'start_date': todo.start_date,
-            'end_date': todo.end_date,
-            'is_completed': todo.is_completed,
-        }
-        return render(request, 'todo_info.html', {'data': info})
-    except Todo.DoesNotExist:
-        raise Http404("Todo does not exist")
+    todo = get_object_or_404(Todo, id=todo_id)
+    context = {
+        'todo': todo.__dict__
+    }
+    return render(request, 'todo_info.html', context)
 
+@login_required
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        todo = form.save(commit=False)
+        todo.user = request.user
+        todo.save()
+        return redirect(reverse('todo_info', kwargs={'todo_id': todo.pk}))
 
+    context = {'form': form}
+    return render(request, 'todo_create.html', context)
+
+@login_required
+def todo_update(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id, user=request.user)
+    form = TodoUpdateForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        form.save()
+        return redirect(reverse('todo_info', kwargs={'todo_id': todo.pk}))
+    context = {'form': form}
+    return render(request, 'todo_update.html', context)
+
+@login_required
+def todo_delete(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id, user=request.user)
+    todo.delete()
+    return redirect(reverse('todo_list'))
